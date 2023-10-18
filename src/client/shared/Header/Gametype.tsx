d5dfa448f01a24255b453ef03f54ff7b6911f33d
timestweb <jobs@timestweb.net>
2023-10-18 08:21:25 +0100

    update: on header section

--- src/client/shared/Header/Gametype.tsx
+++ src/client/shared/Header/Gametype.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React  from 'react'
 import { LINK_GAME_TYPES } from './constant/data';
 import Link from 'next/link';
 import Container from '../../components/Container/Container';
@@ -6,7 +6,6 @@ import { useLink } from '../Hooks/useLink';
 
 const Gametype = () => {
 	const { link, handleClick } = useLink('Basketball');
-
 
   return (
 		<Container bgColor='bg-white'>
@@ -19,7 +18,7 @@ const Gametype = () => {
 						className={`flex items-center justify-center text-center py-4 px-6  transition transform duration-300 ${
 							link === value
 								? 'border-b-blue-900 border-b-4'
-								: 'hover:border-b-blue-400 hover:border-b-4 border-b-white border-b-4'
+								: 'hover:border-b-blue-400 hover:border-b-4 border-b-white border-b-4 hover:text-blue-900'
 						}`}>
 						<p className='font-bold text-sm'>{value}</p>
 					</Link>
