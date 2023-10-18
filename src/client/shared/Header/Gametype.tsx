dc165c70b32641913c8d1fa6d23af90f18eb5971
timestweb <jobs@timestweb.net>
2023-10-18 06:19:31 +0100

    update: on header section

--- src/client/shared/Header/Gametype.tsx
+++ src/client/shared/Header/Gametype.tsx
@@ -19,7 +19,7 @@ const Gametype = () => {
 						className={`flex items-center justify-center text-center py-4 px-6  transition transform duration-300 ${
 							link === value
 								? 'border-b-blue-900 border-b-4'
-								: 'hover:border-b-blue-400 hover:border-b-4 '
+								: 'hover:border-b-blue-400 hover:border-b-4 border-b-white border-b-4'
 						}`}>
 						<p className='font-bold text-sm'>{value}</p>
 					</Link>
