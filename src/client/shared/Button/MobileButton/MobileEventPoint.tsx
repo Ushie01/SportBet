c36869052479ef232241511f5fe9478a0f983406
Ushie01 <abugodwinaj@gmail.com>
2023-11-19 22:55:51 +0100

    update: LiveBettingSection under mobile view

--- src/client/shared/Button/MobileButton/MobileEventPoint.tsx
+++ src/client/shared/Button/MobileButton/MobileEventPoint.tsx
@@ -22,7 +22,28 @@ export const TwoOddsButton = () => {
 
 export const ThreeOddsButton = () => {
   return (
-		<div className='flex w-[170px]'>
+		<div className='flex w-[180px]'>
+			<DropdownButton
+				className='text-green-500 h-9 w-[60px] bg-ash font-bold hover:text-white hover:bg-green-600'
+				items={items}
+			/>
+			<Button
+				text='2.3'
+				classValue='text-green-500 border-l border-l-darkGray h-9 w-[60px] hover:text-white hover:bg-green-600 bg-ash font-bold'
+				link='#'
+			/>
+			<Button
+				text='2.3'
+				classValue='text-green-500 border-l border-l-darkGray h-9 w-[60px] hover:text-white hover:bg-green-600 bg-ash font-bold'
+				link='#'
+			/>
+		</div>
+	);
+}
+
+export const FourOddsButton = () => {
+  return (
+		<div className='flex w-[180px]'>
 			<DropdownButton
 				className='text-green-500 h-9 w-[56.6px] bg-ash font-bold hover:text-white hover:bg-green-600'
 				items={items}
@@ -37,30 +58,9 @@ export const ThreeOddsButton = () => {
 				classValue='text-green-500 border-l border-l-darkGray h-9 w-[56.6px] hover:text-white hover:bg-green-600 bg-ash font-bold'
 				link='#'
 			/>
-		</div>
-	);
-}
-
-export const FourOddsButton = () => {
-  return (
-		<div className='flex w-[200px]'>
-			<DropdownButton
-				className='text-green-500 h-9 w-[52.5px] bg-ash font-bold hover:text-white hover:bg-green-600'
-				items={items}
-			/>
 			<Button
 				text='2.3'
-				classValue='text-green-500 border-l border-l-darkGray h-9 w-[52.5px] hover:text-white hover:bg-green-600 bg-ash font-bold'
-				link='#'
-			/>
-			<Button
-				text='2.3'
-				classValue='text-green-500 border-l border-l-darkGray h-9 w-[52.5px] hover:text-white hover:bg-green-600 bg-ash font-bold'
-				link='#'
-			/>
-			<Button
-				text='2.3'
-				classValue='text-green-500 border-l border-l-darkGray h-9 w-[52.5px] hover:text-white hover:bg-green-600 bg-ash font-bold'
+				classValue='text-green-500 border-l border-l-darkGray h-9 w-[56.6px] hover:text-white hover:bg-green-600 bg-ash font-bold'
 				link='#'
 			/>
 		</div>
