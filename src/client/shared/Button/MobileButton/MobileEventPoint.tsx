720c1a2d65fa1078df5bb1d33d594054dfdb2451
Ushie01 <abugodwinaj@gmail.com>
2023-11-19 22:49:02 +0100

    update: LiveBettingSection under mobile view

--- src/client/shared/Button/MobileButton/MobileEventPoint.tsx
+++ src/client/shared/Button/MobileButton/MobileEventPoint.tsx
@@ -22,19 +22,19 @@ export const TwoOddsButton = () => {
 
 export const ThreeOddsButton = () => {
   return (
-		<div className='flex w-[190px]'>
+		<div className='flex w-[170px]'>
 			<DropdownButton
-				className='text-green-500 h-9 w-[63.3px] bg-ash font-bold hover:text-white hover:bg-green-600'
+				className='text-green-500 h-9 w-[56.6px] bg-ash font-bold hover:text-white hover:bg-green-600'
 				items={items}
 			/>
 			<Button
 				text='2.3'
-				classValue='text-green-500 border-l border-l-darkGray h-9 w-[63.3px] hover:text-white hover:bg-green-600 bg-ash font-bold'
+				classValue='text-green-500 border-l border-l-darkGray h-9 w-[56.6px] hover:text-white hover:bg-green-600 bg-ash font-bold'
 				link='#'
 			/>
 			<Button
 				text='2.3'
-				classValue='text-green-500 border-l border-l-darkGray h-9 w-[63.3px] hover:text-white hover:bg-green-600 bg-ash font-bold'
+				classValue='text-green-500 border-l border-l-darkGray h-9 w-[56.6px] hover:text-white hover:bg-green-600 bg-ash font-bold'
 				link='#'
 			/>
 		</div>
